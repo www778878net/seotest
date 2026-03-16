c49614a7ee3aefe8a91ee73837d99bf63f0715eb
frieda137 <657225485@qq.com>
2026-03-16 16:15:26 +0800

    1

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      // 配置输出目录为out
+      pages: 'out',
+      assets: 'out',
+      fallback: 'index.html'
+    }),
     // 配置路径
     paths: {
       base: ''
